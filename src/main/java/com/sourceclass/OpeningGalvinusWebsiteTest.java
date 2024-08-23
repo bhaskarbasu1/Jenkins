e4737966f29b975d124224a45f6d900327569073
Bhaskar <bhaskarbasu7070@gmail.com>
2024-08-23 14:58:11 +0530

    Added pom and TestNG

---
+++ src/main/java/com/sourceclass/OpeningGalvinusWebsiteTest.java
@@ -0,0 +1,54 @@
+package com.sourceclass;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class OpeningGalvinusWebsiteTest {
+
+    WebDriver mDriver;
+
+    public OpeningGalvinusWebsiteTest(WebDriver driver){
+        this.mDriver=driver;
+    }
+
+    By whatWeDoDropDown = By.xpath("//*[@id=\"menu-item-12230\"]/a");
+    By systemTestingOption = By.xpath("//*[@id=\"menu-item-12230\"]/div/div/div[1]/div/div[3]/div[2]/div[2]/div/ul/li/a/span");
+
+    By nameField = By.id("form-field-name");
+    By emailField = By.id("form-field-email");
+    By companyField = By.xpath("//*[@id=\"form-field-field_d72975f\"]");
+    By countryDropdown = By.xpath("//*[@id=\"form-field-field_73a7a64\"]");
+    By phoneField = By.xpath("//*[@id=\"form-field-field_fb170dd\"]");
+    By logo = By.xpath("//*[@id=\"Objects\"]");
+
+    public void navigateToWhatWeDo() {
+        Actions actions = new Actions(mDriver);
+        WebElement element = mDriver.findElement(whatWeDoDropDown);
+        actions.moveToElement(element).perform();
+        element.click();
+    }
+
+    public void selectSystemTesting() {
+        mDriver.findElement(systemTestingOption).click();
+    }
+
+    public String getPageTitle() {
+        return mDriver.getTitle();
+    }
+
+    public void enterDetails(String name, String email, String company, String country, String phone) {
+        mDriver.findElement(nameField).sendKeys(name);
+        mDriver.findElement(emailField).sendKeys(email);
+        mDriver.findElement(companyField).sendKeys(company);
+        Select select = new Select(mDriver.findElement(countryDropdown));
+        select.selectByValue(country);
+        mDriver.findElement(phoneField).sendKeys(phone);
+    }
+
+    public void clickOnLogo() {
+        mDriver.findElement(logo).click();
+    }
+}
---
+++ src/test/java/com/testclass/ActionClassOfOpeningWebsite.java
@@ -0,0 +1,55 @@
+package com.testclass;
+
+import com.sourceclass.OpeningGalvinusWebsiteTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ActionClassOfOpeningWebsite {
+
+    public static WebDriver mDriver;
+    public static OpeningGalvinusWebsiteTest openingGalvinusWebsiteTest;
+
+    @BeforeClass
+    public void initializers() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        mDriver = new ChromeDriver();
+        openingGalvinusWebsiteTest = new OpeningGalvinusWebsiteTest(mDriver);
+        mDriver.get("https://galvinus.com/");
+        mDriver.manage().window().maximize();
+    }
+
+    @Test(priority = 1)
+    public void testWhatWeDoOption() throws InterruptedException {
+       openingGalvinusWebsiteTest.navigateToWhatWeDo();
+        Thread.sleep(2000);
+    }
+
+    @Test(priority = 2)
+    public void testSystemTesting() throws InterruptedException {
+        openingGalvinusWebsiteTest.selectSystemTesting();
+    }
+
+    @Test(priority = 3)
+    public void testTitleOfThePage() throws InterruptedException {
+        System.out.println(openingGalvinusWebsiteTest.getPageTitle());
+    }
+
+    @Test(priority = 4)
+    public void testEnterDetails() throws InterruptedException {
+        openingGalvinusWebsiteTest.enterDetails("Bhaskar", "basubhaskar14@gmail.com", "Galvinus", "India", "123456789");
+        Thread.sleep(3000);
+    }
+
+    @Test(priority = 5)
+    public void testClickOnLogo() {
+        openingGalvinusWebsiteTest.clickOnLogo();
+    }
+
+    @AfterClass
+    public void closeTheBrowser() {
+        mDriver.quit();
+    }
+}
